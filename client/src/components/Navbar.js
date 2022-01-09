6c659425d0158739bfc5a2ab4c12736294cd568d
HacAtac <jhack00@icloud.com>
2022-01-09 17:19:08 -0600

    nav bar functionality

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,17 +2,57 @@ import React from "react";
 
 function Navbar() {
   const user = JSON.parse(localStorage.getItem("currentuser"));
+  function logout() {
+    localStorage.removeItem("currentuser");
+    window.location.href = "/login";
+  }
   return (
     <div>
       <nav className="navbar navbar-expand-lg">
         <a className="navbar-brand" href="/">
           HacAtac Rooms
         </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span
+            className="navbar-toggler-icon bg-gray"
+            style={{ color: "white" }}
+          ></span>
+        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav mr-5 pr-5">
             {user ? (
               <>
-                <h1 style={{ color: "white" }}>{user.name}</h1>
+                <div className="dropdown">
+                  <button
+                    className="btn btn-secondary dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    {user.name}
+                  </button>
+                  <div
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton"
+                  >
+                    <a className="dropdown-item" href="#">
+                      Bookings
+                    </a>
+                    <a className="dropdown-item" href="#" onClick={logout}>
+                      Logout
+                    </a>
+                  </div>
+                </div>
               </>
             ) : (
               <>
