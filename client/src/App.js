3045bace046b0fe1b0797aca29f73d52787edaab
HacAtac <jhack00@icloud.com>
2022-01-09 13:48:52 -0600

    front end signup login done

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import Homescreen from "./screens/Homescreen";
 import Bookingscreen from "./screens/Bookingscreen";
+import Registerscreen from "./screens/Registerscreen";
+import Loginscreen from "./screens/Loginscreen";
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,8 @@ function App() {
       <BrowserRouter>
         <Route path="/" exact component={Homescreen} />
         <Route path="/book/:roomid" exact component={Bookingscreen} />
+        <Route path="/register" exact component={Registerscreen} />
+        <Route path="/login" exact component={Loginscreen} />
       </BrowserRouter>
     </div>
   );
